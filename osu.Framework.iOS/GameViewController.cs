bb31c72f5ef398a0ff73e2cb51174b0759e5fe19
Salman Ahmed <frenzibyte@gmail.com>
2021-12-05 09:21:49 +0300

    Expand game view to its largest square before transition

--- osu.Framework.iOS/GameViewController.cs
+++ osu.Framework.iOS/GameViewController.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using CoreGraphics;
 using osu.Framework.Platform;
 using UIKit;
@@ -33,6 +34,18 @@ namespace osu.Framework.iOS
         public override void ViewWillTransitionToSize(CGSize toSize, IUIViewControllerTransitionCoordinator coordinator)
         {
             base.ViewWillTransitionToSize(toSize, coordinator);
+
+            if (toSize == gameView.Bounds.Size)
+                return;
+
+            // expand the game view to the largest square of its pre-transition size,
+            // so that the scaling of the gl viewport during transition doesn't look jarring.
+            var bounds = gameView.Bounds;
+            bounds.Width = NMath.Max(bounds.Width, bounds.Height);
+            bounds.Height = NMath.Max(bounds.Width, bounds.Height);
+            gameView.Bounds = bounds;
+
+            coordinator.AnimateAlongsideTransition(_ => gameView.Bounds = new CGRect(CGPoint.Empty, toSize), null);
         }
     }
 }
