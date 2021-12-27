95347e6b97f0f706e4dd96d29c4f02620bbabd1f
Salman Ahmed <frenzibyte@gmail.com>
2021-12-27 18:47:53 +0300

    Fix blur shader not updated

--- osu.Framework/Resources/Shaders/sh_Blur.fs
+++ osu.Framework/Resources/Shaders/sh_Blur.fs
@@ -4,7 +4,8 @@
 
 layout(location = 2) in mediump vec2 v_TexCoord;
 
-uniform lowp sampler2D m_Sampler;
+uniform lowp texture2D m_Texture;
+uniform lowp sampler m_Sampler;
 
 uniform mediump vec2 g_TexSize;
 uniform int g_Radius;
@@ -18,10 +19,10 @@ mediump float computeGauss(in mediump float x, in mediump float sigma)
 	return INV_SQRT_2PI * exp(-0.5*x*x / (sigma*sigma)) / sigma;
 }
 
-lowp vec4 blur(sampler2D tex, int radius, highp vec2 direction, mediump vec2 texCoord, mediump vec2 texSize, mediump float sigma)
+lowp vec4 blur(texture2D tex, sampler samp, int radius, highp vec2 direction, mediump vec2 texCoord, mediump vec2 texSize, mediump float sigma)
 {
 	mediump float factor = computeGauss(0.0, sigma);
-	mediump vec4 sum = texture2D(tex, texCoord) * factor;
+	mediump vec4 sum = texture(sampler2D(tex, samp), texCoord) * factor;
 
 	mediump float totalFactor = factor;
 
@@ -30,8 +31,8 @@ lowp vec4 blur(sampler2D tex, int radius, highp vec2 direction, mediump vec2 tex
 		mediump float x = float(i) - 0.5;
 		factor = computeGauss(x, sigma) * 2.0;
 		totalFactor += 2.0 * factor;
-		sum += texture2D(tex, texCoord + direction * x / texSize) * factor;
-		sum += texture2D(tex, texCoord - direction * x / texSize) * factor;
+		sum += texture(sampler2D(tex, samp), texCoord + direction * x / texSize) * factor;
+		sum += texture(sampler2D(tex, samp), texCoord - direction * x / texSize) * factor;
 		if (i >= radius)
 			break;
 	}
@@ -41,5 +42,5 @@ lowp vec4 blur(sampler2D tex, int radius, highp vec2 direction, mediump vec2 tex
 
 void main(void)
 {
-	o_Colour = blur(m_Sampler, g_Radius, g_BlurDirection, v_TexCoord, g_TexSize, g_Sigma);
+	o_Colour = blur(m_Texture, m_Sampler, g_Radius, g_BlurDirection, v_TexCoord, g_TexSize, g_Sigma);
 }
