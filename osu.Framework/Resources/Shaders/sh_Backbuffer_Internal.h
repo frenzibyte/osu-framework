cd7cdc446f0108f58fdef66638ddfab42729cd38
Salman Ahmed <frenzibyte@gmail.com>
2022-02-24 18:24:44 +0300

    Add reason for `location = -1` in backbuffer header

--- osu.Framework/Resources/Shaders/sh_Backbuffer_Internal.h
+++ osu.Framework/Resources/Shaders/sh_Backbuffer_Internal.h
@@ -1,6 +1,8 @@
 // Automatically included for every vertex shader.
 
-layout(location = -1) in float m_BackbufferDrawDepth;
+// The -1 is a placeholder value to offset all vertex input members
+// of the actual vertex shader during inclusion of this header.
+layout(location = -1) in highp float m_BackbufferDrawDepth;
 
 // Whether the backbuffer is currently being drawn to
 uniform bool g_BackbufferDraw;
