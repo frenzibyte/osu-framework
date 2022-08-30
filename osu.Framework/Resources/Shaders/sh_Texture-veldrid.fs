ca3ea29b75b8ed5d134d1797aa89b8c614a8cbde
Salman Ahmed <frenzibyte@gmail.com>
2022-08-30 14:26:26 +0300

    Add initial veldrid shader resources

---
+++ osu.Framework/Resources/Shaders/sh_Texture-veldrid.fs
@@ -0,0 +1,18 @@
+#include "sh_Utils.h"
+#include "sh_TextureWrapping.h"
+
+layout(location = 0) in highp vec2 v_MaskingPosition;
+layout(location = 1) in lowp vec4 v_Colour;
+layout(location = 2) in mediump vec2 v_TexCoord;
+layout(location = 3) in mediump vec4 v_TexRect;
+layout(location = 4) in mediump vec2 v_BlendRange;
+
+layout(set = 1, binding = 0) uniform lowp texture2D m_Texture;
+layout(set = 1, binding = 1) uniform lowp sampler m_Sampler;
+
+layout(location = 0) out vec4 o_Colour;
+
+void main()
+{
+    o_Colour = toSRGB(v_Colour * wrappedTexture(wrap(v_TexCoord, v_TexRect), v_TexRect, m_Texture, m_Sampler, -0.9));
+}
