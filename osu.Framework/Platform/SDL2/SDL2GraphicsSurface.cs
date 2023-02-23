bdc35614ef2d5fe00aa8d58e6293b3f3a1f77697
Salman Ahmed <frenzibyte@gmail.com>
2023-02-23 15:02:55 +0300

    Refactor iOS application startup logic to power up SDL

--- osu.Framework/Platform/SDL2/SDL2GraphicsSurface.cs
+++ osu.Framework/Platform/SDL2/SDL2GraphicsSurface.cs
@@ -83,7 +83,14 @@ namespace osu.Framework.Platform.SDL2
 
         private void initialiseOpenGL()
         {
-            SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_PROFILE_MASK, SDL.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+            if (RuntimeInfo.IsMobile)
+            {
+                SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_PROFILE_MASK, SDL.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_ES);
+                SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+                SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_MINOR_VERSION, 0);
+            }
+            else
+                SDL.SDL_GL_SetAttribute(SDL.SDL_GLattr.SDL_GL_CONTEXT_PROFILE_MASK, SDL.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 
             context = SDL.SDL_GL_CreateContext(window.SDLWindowHandle);
 
@@ -152,7 +159,11 @@ namespace osu.Framework.Platform.SDL2
         bool IOpenGLGraphicsSurface.VerticalSync
         {
             get => SDL.SDL_GL_GetSwapInterval() != 0;
-            set => SDL.SDL_GL_SetSwapInterval(value ? 1 : 0);
+            set
+            {
+                if (RuntimeInfo.IsDesktop)
+                    SDL.SDL_GL_SetSwapInterval(value ? 1 : 0);
+            }
         }
 
         IntPtr IOpenGLGraphicsSurface.WindowContext => context;
