032200c1ac537b45671e174bca7b90bd6da23d86
Salman Ahmed <frenzibyte@gmail.com>
2023-03-25 15:11:56 +0300

    Add sample game boxes

--- SampleGame/SampleGameGame.cs
+++ SampleGame/SampleGameGame.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Diagnostics;
 using osu.Framework;
 using osu.Framework.Graphics;
 using osuTK;
 using osuTK.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
 
 namespace SampleGame
 {
@@ -17,6 +19,35 @@ namespace SampleGame
         [BackgroundDependencyLoader]
         private void load()
         {
+            Add(box = new Box
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(300, 300),
+                Colour = Color4.Green
+            });
+            Add(new BufferedContainer(cachedFrameBuffer: false)
+            {
+                AutoSizeAxes = Axes.Both,
+                Child = box = new Box
+                {
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Size = new Vector2(150, 150),
+                    Colour = Color4.SkyBlue
+                }
+            });
+            Add(new BufferedContainer(cachedFrameBuffer: false)
+            {
+                AutoSizeAxes = Axes.Both,
+                Anchor = Anchor.TopRight,
+                Origin = Anchor.TopRight,
+                Child = box = new Box
+                {
+                    Size = new Vector2(150, 150),
+                    Colour = Color4.Blue
+                }
+            });
             Add(box = new Box
             {
                 Anchor = Anchor.Centre,
