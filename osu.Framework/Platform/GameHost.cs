372b3d133f9eb8edda37088cb0580754c9084b74
Salman Ahmed <frenzibyte@gmail.com>
2023-03-26 01:32:36 +0300

    Remove usage of vertex batches

--- osu.Framework/Platform/GameHost.cs
+++ osu.Framework/Platform/GameHost.cs
@@ -33,6 +33,7 @@ using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.OpenGL;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Rendering.Vertices;
 using osu.Framework.Input;
 using osu.Framework.Input.Bindings;
 using osu.Framework.Input.Handlers;
@@ -54,6 +55,7 @@ using osu.Framework.Testing;
 using osuTK.Graphics;
 using Veldrid;
 using Veldrid.SPIRV;
+using BufferUsage = Veldrid.BufferUsage;
 using Image = SixLabors.ImageSharp.Image;
 using PixelFormat = osuTK.Graphics.ES30.PixelFormat;
 using Size = System.Drawing.Size;
@@ -484,42 +486,30 @@ namespace osu.Framework.Platform
         private VeldridFrameBuffer frameBuffer;
 
         private Game game;
-        private Shader[] shaders;
         private bool initialised;
+
+        private Shader[] shaders;
+        private VeldridVertexBuffer<Vertex2D> vertexBuffer;
+        private VeldridVertexBuffer<Vertex2D> vertexBuffer2;
 
         private const string vertex_shader = @"
 #version 450
 
 layout(location = 0) in highp vec2 m_Position;
 layout(location = 1) in lowp vec4 m_Colour;
-layout(location = 2) in highp vec2 m_TexCoord;
-layout(location = 3) in highp vec4 m_TexRect;
-layout(location = 4) in mediump vec2 m_BlendRange;
 
-layout(location = 0) out highp vec2 v_MaskingPosition;
-layout(location = 1) out lowp vec4 v_Colour;
-layout(location = 2) out highp vec2 v_TexCoord;
-layout(location = 3) out highp vec4 v_TexRect;
-layout(location = 4) out mediump vec2 v_BlendRange;
+layout(location = 0) out lowp vec4 v_Colour;
 
 void main(void)
 {
-	// Transform from screen space to masking space.
-	highp vec3 maskingPos = vec3(m_Position, 1.0);
-	v_MaskingPosition = maskingPos.xy / maskingPos.z;
-
 	v_Colour = m_Colour;
-	v_TexCoord = m_TexCoord;
-	v_TexRect = m_TexRect;
-	v_BlendRange = m_BlendRange;
-
 	gl_Position = vec4(m_Position, 0.5, 1.0);
 }";
 
         private const string fragment_shader = @"
 #version 450
 
-layout(location = 1) in lowp vec4 v_Colour;
+layout(location = 0) in lowp vec4 v_Colour;
 
 layout(location = 0) out vec4 o_Colour;
 
@@ -536,6 +526,9 @@ void main(void)
             shaders = veldrid.Device.ResourceFactory.CreateFromSpirv(
                 new ShaderDescription(ShaderStages.Vertex, Encoding.UTF8.GetBytes(vertex_shader), "main0"),
                 new ShaderDescription(ShaderStages.Fragment, Encoding.UTF8.GetBytes(fragment_shader), "main0"));
+
+            vertexBuffer = new VeldridQuadBuffer<Vertex2D>(veldrid, 1, BufferUsage.Dynamic);
+            vertexBuffer2 = new VeldridQuadBuffer<Vertex2D>(veldrid, 1, BufferUsage.Dynamic);
 
             initialised = true;
         }
@@ -591,15 +584,21 @@ void main(void)
 
                 renderer.pipeline.ShaderSet.Shaders = shaders;
 
-                renderer.DrawQuad(renderer.WhitePixel, new Quad(-0.75f, -0.75f, 0.75f * 2, 0.75f * 2), Color4.Red);
+                vertexBuffer.SetVertex(0, new Vertex2D { Position = new Vector2(-0.75f, 0.75f), Colour = Color4.Red });
+                vertexBuffer.SetVertex(1, new Vertex2D { Position = new Vector2(0.75f, 0.75f), Colour = Color4.Red });
+                vertexBuffer.SetVertex(2, new Vertex2D { Position = new Vector2(0.75f, -0.75f), Colour = Color4.Red });
+                vertexBuffer.SetVertex(3, new Vertex2D { Position = new Vector2(-0.75f, -0.75f), Colour = Color4.Red });
+                vertexBuffer2.SetVertex(0, new Vertex2D { Position = new Vector2(-0.25f, 0.25f), Colour = Color4.Blue });
+                vertexBuffer2.SetVertex(1, new Vertex2D { Position = new Vector2(0.25f, 0.25f), Colour = Color4.Blue });
+                vertexBuffer2.SetVertex(2, new Vertex2D { Position = new Vector2(0.25f, -0.25f), Colour = Color4.Blue });
+                vertexBuffer2.SetVertex(3, new Vertex2D { Position = new Vector2(-0.25f, -0.25f), Colour = Color4.Blue });
 
-                renderer.FlushCurrentBatch(FlushBatchSource.SetShader);
+                vertexBuffer.UpdateRange(0, 4);
+                vertexBuffer2.UpdateRange(0, 4);
 
-                // renderer.Commands.ResetRenderPass();
-
-                renderer.DrawQuad(renderer.WhitePixel, new Quad(-0.25f, -0.25f, 0.25f * 2, 0.25f * 2), Color4.Blue);
-
-                renderer.FlushCurrentBatch(FlushBatchSource.SetShader);
+                vertexBuffer.DrawRange(0, 4);
+                renderer.Commands.ResetRenderPass();
+                vertexBuffer2.DrawRange(0, 4);
 
                 // Back pass
                 // buffer.Object.Draw(Renderer);
